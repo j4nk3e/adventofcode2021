672dc7ee65bc4c40979a9cdea8373c41f2df539b
Julian Köpke <julian.koepke@gmail.com>
2021-12-12 15:40:41 +0100

    a12

---
+++ a12a/src/main.rs
@@ -0,0 +1,39 @@
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+};
+
+fn main() {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+    let mut caves: HashMap<String, Vec<String>> = HashMap::new();
+    for line in lines {
+        let l = line.unwrap();
+        let conn: Vec<String> = l.split("-").map(str::to_string).collect();
+        for cave in conn.iter() {
+            let other = conn.iter().filter(|c| *c != "start" && *c != cave).next();
+            let c = caves.entry(cave.to_string()).or_insert(Vec::new());
+            if let Some(o) = other {
+                c.push(o.to_string());
+            }
+        }
+    }
+
+    let count = find_paths(&caves, "start".to_string(), "start".to_string());
+    println!("{}", count);
+}
+
+fn find_paths(caves: &HashMap<String, Vec<String>>, path: String, pos: String) -> u64 {
+    if pos == "end" {
+        return 1;
+    }
+    let mut count = 0;
+    for p in caves.get(&pos).unwrap().iter() {
+        if p.chars().next().unwrap().is_lowercase() && path.contains(p) {
+            continue;
+        }
+        let new_path = path.to_owned() + "-" + p;
+        count += find_paths(caves, new_path, p.to_owned());
+    }
+    return count;
+}
