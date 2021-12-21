333739c6c3dc5eba8aefac983bc12f33933d5707
Julian Köpke <julian.koepke@gmail.com>
2021-12-21 01:48:04 +0100

    a20 speed optimizations

--- a20b/src/main.rs
+++ a20b/src/main.rs
@@ -1,23 +1,18 @@
 use std::{
-    collections::{HashMap, HashSet},
     error::Error,
     io::{self, BufRead},
 };
+
+use rustc_hash::FxHashMap;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
     let mut max_x = 0;
     let mut max_y = 0;
-    let algo: HashSet<usize> = lines
-        .next()
-        .unwrap()?
-        .chars()
-        .enumerate()
-        .filter_map(|(p, b)| if b == '#' { Some(p) } else { None })
-        .collect();
+    let algo: Vec<bool> = lines.next().unwrap()?.chars().map(|c| c == '#').collect();
     lines.next();
-    let mut image: HashMap<(isize, isize), bool> = HashMap::new();
+    let mut image: FxHashMap<(isize, isize), bool> = FxHashMap::default();
     for (y, l) in lines.enumerate() {
         for (x, c) in l?.chars().map(|c| c == '#').enumerate() {
             if c {
@@ -29,48 +24,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     let mut background = false;
     for i in 1..51 {
-        let mut next: HashMap<(isize, isize), bool> = HashMap::new();
+        let mut next: FxHashMap<(isize, isize), bool> = FxHashMap::default();
         for y in -i * 2..max_y + i * 2 {
             for x in -i * 2..max_x + i * 2 {
                 let mut q = 0;
                 let mut shift = 0;
                 for dy in -1..2 {
                     for dx in -1..2 {
-                        if *image.get(&(x + dx, y + dy)).unwrap_or(&background) {
+                        if *image.get(&(x - dx, y - dy)).unwrap_or(&background) {
                             q |= 1 << shift;
                         };
                         shift += 1;
                     }
                 }
-                next.insert((x, y), algo.contains(&q));
+                next.insert((x, y), algo[q]);
             }
         }
-        image = next;
-        if !background && algo.contains(&0) {
+        if !background && algo[0] {
             background = true;
-        } else if background && !algo.contains(&0b111111111) {
+        } else if background && !algo[0b111111111] {
             background = false;
         }
-        image.retain(|_, v| background != *v);
-
-        // for y in -i * 2..max_y + i * 2 {
-        //     for x in -i * 2..max_x + i * 2 {
-        //         print!(
-        //             "{}",
-        //             if *image.get(&(x, y)).unwrap_or_else(|| &background) {
-        //                 '#'
-        //             } else {
-        //                 '.'
-        //             }
-        //         );
-        //     }
-        //     println!();
-        // }
-        println!(
-            "{}: {}",
-            i,
-            image.iter().filter(|(_, v)| **v != background).count()
-        );
+        next.retain(|_, v| background != *v);
+        image = next;
     }
+    println!("{}", image.len());
     Ok(())
 }
