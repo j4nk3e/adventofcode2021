69b73810bc8e3322805c81cba1d5cc3eaed73397
Julian Köpke <julian.koepke@gmail.com>
2021-12-25 17:21:01 +0100

    a25 optimized

--- a25a/src/main.rs
+++ a25a/src/main.rs
@@ -1,50 +1,61 @@
-use std::{
-    collections::HashSet,
-    io::{self, BufRead},
-};
+use std::io::{self, BufRead};
+
+use bitvec::{bitvec, order::Lsb0, prelude::BitVec};
 
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
     let mut w = 0;
-    let mut h = 0;
-    let mut e: HashSet<(usize, usize)> = HashSet::new();
-    let mut s: HashSet<(usize, usize)> = HashSet::new();
+    let mut e: BitVec<Lsb0, usize> = bitvec![];
+    let mut s: BitVec<Lsb0, usize> = bitvec![];
     for line in lines {
         let l = line.unwrap();
         w = l.len();
-        l.chars().enumerate().for_each(|(x, c)| {
-            match c {
-                '>' => e.insert((x, h)),
-                'v' => s.insert((x, h)),
-                _ => true,
+        l.chars().for_each(|c| {
+            let (eb, sb) = match c {
+                '>' => (true, false),
+                'v' => (false, true),
+                _ => (false, false),
             };
+            e.push(eb);
+            s.push(sb);
         });
-        h += 1;
     }
     let mut i = 0;
     loop {
         i += 1;
         let mut m = 0;
-        let mut n_e: HashSet<(usize, usize)> = HashSet::new();
-        for (x, y) in e.iter() {
-            let t = ((x + 1) % w, *y);
-            n_e.insert(if e.contains(&t) || s.contains(&t) {
-                (*x, *y)
-            } else {
-                m += 1;
-                t
-            });
+        let mut n_e: BitVec<Lsb0, usize> = BitVec::with_capacity(e.len());
+        let mut n_s: BitVec<Lsb0, usize> = BitVec::with_capacity(s.len());
+        n_e.resize(e.len(), false);
+        n_s.resize(s.len(), false);
+        for p in e.iter_ones() {
+            let x = p % w;
+            let y = p / w;
+            let t = (x + 1) % w + y * w;
+            n_e.set(
+                if e[t] || s[t] {
+                    p
+                } else {
+                    m += 1;
+                    t
+                },
+                true,
+            );
         }
-        let mut n_s: HashSet<(usize, usize)> = HashSet::new();
-        for (x, y) in s.iter() {
-            let t = (*x, (*y + 1) % h);
-            n_s.insert(if n_e.contains(&t) || s.contains(&t) {
-                (*x, *y)
-            } else {
-                m += 1;
-                t
-            });
+        for p in s.iter_ones() {
+            let x = p % w;
+            let y = p / w;
+            let t = x + ((y + 1) % (s.len() / w)) * w;
+            n_s.set(
+                if n_e[t] || s[t] {
+                    p
+                } else {
+                    m += 1;
+                    t
+                },
+                true,
+            );
         }
         if m == 0 {
             println!("{}", i);
