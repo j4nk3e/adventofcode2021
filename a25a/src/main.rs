6dcb626693d5ff7f890b4b4f9dc6c0d25b4ab286
Julian Köpke <julian.koepke@gmail.com>
2021-12-25 18:36:13 +0100

    a25 with rustc-hash

--- a25a/src/main.rs
+++ a25a/src/main.rs
@@ -1,67 +1,62 @@
 use std::io::{self, BufRead};
 
-use bitvec::{bitvec, order::Lsb0, prelude::BitVec};
+use rustc_hash::FxHashSet;
 
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
     let mut w = 0;
-    let mut e: BitVec<Lsb0, usize> = bitvec![];
-    let mut s: BitVec<Lsb0, usize> = bitvec![];
+    let mut e: FxHashSet<usize> = FxHashSet::default();
+    let mut s: FxHashSet<usize> = FxHashSet::default();
+    let mut i = 0;
     for line in lines {
         let l = line.unwrap();
         w = l.len();
         l.chars().for_each(|c| {
-            let (eb, sb) = match c {
-                '>' => (true, false),
-                'v' => (false, true),
-                _ => (false, false),
+            match c {
+                '>' => e.insert(i),
+                'v' => s.insert(i),
+                _ => true,
             };
-            e.push(eb);
-            s.push(sb);
+            i += 1;
         });
     }
+    let h = i / w;
     let mut i = 0;
     loop {
         i += 1;
-        let mut m = 0;
-        let mut n_e: BitVec<Lsb0, usize> = BitVec::with_capacity(e.len());
-        let mut n_s: BitVec<Lsb0, usize> = BitVec::with_capacity(s.len());
-        n_e.resize(e.len(), false);
-        n_s.resize(s.len(), false);
-        for p in e.iter_ones() {
-            let x = p % w;
-            let y = p / w;
-            let t = (x + 1) % w + y * w;
-            n_e.set(
-                if e[t] || s[t] {
-                    p
+        let mut fin = true;
+        e = e
+            .iter()
+            .map(|p| {
+                let x = p % w;
+                let y = p / w;
+                let t = (x + 1) % w + y * w;
+                if e.contains(&t) || s.contains(&t) {
+                    *p
                 } else {
-                    m += 1;
+                    fin = false;
                     t
-                },
-                true,
-            );
-        }
-        for p in s.iter_ones() {
-            let x = p % w;
-            let y = p / w;
-            let t = x + ((y + 1) % (s.len() / w)) * w;
-            n_s.set(
-                if n_e[t] || s[t] {
-                    p
+                }
+            })
+            .collect();
+        s = s
+            .iter()
+            .map(|p| {
+                let x = p % w;
+                let y = p / w;
+                let t = x + ((y + 1) % h) * w;
+                if e.contains(&t) || s.contains(&t) {
+                    *p
                 } else {
-                    m += 1;
+                    fin = false;
                     t
-                },
-                true,
-            );
-        }
-        if m == 0 {
+                }
+            })
+            .collect();
+        if fin {
             println!("{}", i);
             break;
         }
-        e = n_e;
-        s = n_s;
     }
 }
